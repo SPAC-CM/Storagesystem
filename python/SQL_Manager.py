ec25b42af156e3498de1ce1188d1b94f2733a97a
Christoffer Møldrup <a-spac02@specialisterne.dk>
2024-11-15 11:16:41 +0100

    uploading current

--- python/SQL_Manager.py
+++ python/SQL_Manager.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from sqlalchemy.orm.decl_api import DeclarativeMeta
-from Catagory import Catagory
 class SQL_Manager(object):
 
     _instance = None
@@ -10,6 +9,7 @@ class SQL_Manager(object):
         if not isinstance(cls._instance,cls):
             cls._instance = object.__new__(cls)
         return cls._instance
+    
     def __init__(self):
         engine_uri = f"mysql+pymysql://{os.getenv('mysqluser')}:{os.getenv('mysqlpass')}@{os.getenv('mysqlhost')}/Products"
         Session = sessionmaker(bind = create_engine(engine_uri))
@@ -18,13 +18,12 @@ class SQL_Manager(object):
     def add_table(self, table_name : str):
         print("horse")
 
-
     def add_item(self, item : DeclarativeMeta):
         self.session.add(item)
         self.session.commit()
 
 
 if __name__ == '__main__':
-    catagory = Catagory(name="Stuff")
+    catagory = Category(name="Stuff")
     manager = SQL_Manager()
     manager.add_item(catagory)
